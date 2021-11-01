51d3c5b757052a9dfcac6fdbe9ffde4840ad3cab
JunHoBaek <bemodesty306@naver.com>
2021-11-01 23:24:20 +0900

    21.11.01 db practice

--- client/src/network/http.js
+++ client/src/network/http.js
@@ -1,26 +1,33 @@
 export default class HttpClient {
-  constructor(baseURL) {
+  constructor(baseURL, authErrorEventBus) {
     this.baseURL = baseURL;
+    this.authErrorEventBus = authErrorEventBus;
   }
 
   async fetch(url, options) {
-    const response = await fetch(`${this.baseURL}${url}`, {
+    const res = await fetch(`${this.baseURL}${url}`, {
       ...options,
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
         ...options.headers,
       },
     });
     let data;
     try {
-      data = await response.json();
+      data = await res.json();
     } catch (error) {
-      console.log(error);
+      console.error(error);
     }
-    if (response.status > 299 || response.status < 200) {
+
+    if (res.status > 299 || res.status < 200) {
       const message =
-        data && data.message ? data.message : "Somthing went wrong!";
-      throw new Error(message);
+        data && data.message ? data.message : 'Something went wrong! 🤪';
+      const error = new Error(message);
+      if (res.status === 401) {
+        this.authErrorEventBus.notify(error);
+        return;
+      }
+      throw error;
     }
     return data;
   }
--- server/app.js
+++ server/app.js
@@ -4,6 +4,9 @@ import cors from 'cors';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import tweetsRouter from './router/tweets.js';
+import authRouter from './router/auth.js';
+import { config } from './config.js';
+import { initSocket } from './connection/socket.js';
 
 const app = express();
 
@@ -13,6 +16,7 @@ app.use(cors());
 app.use(morgan('tiny'));
 
 app.use('/tweets', tweetsRouter);
+app.use('/auth', authRouter);
 
 app.use((req, res, next) => {
   res.sendStatus(404);
@@ -22,4 +26,6 @@ app.use((error, req, res, next) => {
   console.error(error);
   res.sendStatus(500);
 });
-app.listen(8080);
+
+const server = app.listen(config.host.port);
+initSocket(server);
