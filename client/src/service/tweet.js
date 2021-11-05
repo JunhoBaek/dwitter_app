e6bddbcba0ee13801fdcc842973c7b48cef707ba
JunHoBaek <bemodesty306@naver.com>
2021-11-06 00:03:31 +0900

    프론트엔드 연결

--- client/src/service/tweet.js
+++ client/src/service/tweet.js
@@ -1,31 +1,21 @@
 export default class TweetService {
-  tweets = [
-    {
-      id: 1,
-      text: '드림코딩에서 강의 들으면 너무 좋으다',
-      createdAt: '2021-05-09T04:20:57.000Z',
-      name: 'Bob',
-      username: 'bob',
-      url: 'https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png',
-    },
-  ];
-
+  constructor(baseURL) {
+    this.baseURL = baseURL;
+    this.tweetsURL = baseURL + "/tweets";
+  }
   async getTweets(username) {
-    return username
-      ? this.tweets.filter((tweet) => tweet.username === username)
-      : this.tweets;
+    const res = await fetch(this.tweetsURL, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+    return data;
   }
 
   async postTweet(text) {
-    const tweet = {
-      id: Date.now(),
-      createdAt: new Date(),
-      name: 'Ellie',
-      username: 'ellie',
-      text,
-    };
-    this.tweets.push(tweet);
-    return tweet;
+    //todo
   }
 
   async deleteTweet(tweetId) {
@@ -35,7 +25,7 @@ export default class TweetService {
   async updateTweet(tweetId, text) {
     const tweet = this.tweets.find((tweet) => tweet.id === tweetId);
     if (!tweet) {
-      throw new Error('tweet not found!');
+      throw new Error("tweet not found!");
     }
     tweet.text = text;
     return tweet;
--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 import tweetRouter from "./router/tweet.js";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(morgan("tiny"));
 
 app.use("/tweets", tweetRouter);
